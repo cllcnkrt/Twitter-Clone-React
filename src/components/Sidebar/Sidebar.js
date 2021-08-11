d0e4ecf0479bd1d45bc7c89af336c4e37df6860a
cllcnkrt <cllcnkrt@outlook.com>
2021-08-11 10:46:57 +0300

    sidebarOptions component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
 function Sidebar() {
   return (
     <div className="sidebar">
       {/* twitter icon */}
       <TwitterIcon color="primary" />
-      {/* SidebarOption */}
-      {/* SidebarOption */} 
-      {/* SidebarOption */} 
-      {/* SidebarOption */}
+
+      {/* SidebarOptions */}
+      <SidebarOption Icon={HomeIcon} text="Home"/>
+  
+      <SidebarOption Icon={SearchIcon} text="Explore"/>
+    
+       <SidebarOption Icon={HomeIcon} text="Notifications"/> 
+     
 
       {/* Button -> Tweet */}
     </div>
--- src/components/Sidebar/SidebarOption.js
+++ src/components/Sidebar/SidebarOption.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import './SidebarOption.css'
 
-function SidebarOption() {
+
+function SidebarOption({text, Icon}) {
     return (
         <div className="sidebarOption">
             <h2>This is an option</h2>
