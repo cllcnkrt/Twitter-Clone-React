7907bdd719ba3e584560760eee3c3b90d143d48b
cllcnkrt <cllcnkrt@outlook.com>
2021-08-11 10:22:51 +0300

    sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* twitter icon */}
+      <TwitterIcon color="primary" />
+      {/* SidebarOption */}
+      {/* SidebarOption */} 
+      {/* SidebarOption */} 
+      {/* SidebarOption */}
+
+      {/* Button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
